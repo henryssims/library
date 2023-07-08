3ad20934936f19077c7db6da1353dc18a14fcc0b
henryssims <134118548+henryssims@users.noreply.github.com>
2023-07-08 19:39:52 -0400

    class

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let library = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;    
+    }
 
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
 const container = document.getElementById("container");
